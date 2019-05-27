ae46b4d42804fdc62d532d3ab7e97b5ea98999f3
moezzie <sebastian@moeser.se>
2019-05-27 03:47:15 +0200

    Generic request handler

--- app/app.go
+++ app/app.go
@@ -41,22 +41,22 @@ func (a *App) Initialize(config *config.Config) {
 // setRouters sets the all required routers
 func (a *App) setRouters() {
 	// Routing for handling the projects
-	a.Get("/projects", a.GetAllProjects)
-	a.Post("/projects", a.CreateProject)
-	a.Get("/projects/{title}", a.GetProject)
-	a.Put("/projects/{title}", a.UpdateProject)
-	a.Delete("/projects/{title}", a.DeleteProject)
-	a.Put("/projects/{title}/archive", a.ArchiveProject)
-	a.Delete("/projects/{title}/archive", a.RestoreProject)
+	a.Get("/projects", a.handleRequest(handler.GetAllProjects))
+	a.Post("/projects", a.handleRequest(handler.CreateProject))
+	a.Get("/projects/{title}", a.handleRequest(handler.GetProject))
+	a.Put("/projects/{title}", a.handleRequest(handler.UpdateProject))
+	a.Delete("/projects/{title}", a.handleRequest(handler.DeleteProject))
+	a.Put("/projects/{title}/archive", a.handleRequest(handler.ArchiveProject))
+	a.Delete("/projects/{title}/archive", a.handleRequest(handler.RestoreProject))
 
 	// Routing for handling the tasks
-	a.Get("/projects/{title}/tasks", a.GetAllTasks)
-	a.Post("/projects/{title}/tasks", a.CreateTask)
-	a.Get("/projects/{title}/tasks/{id:[0-9]+}", a.GetTask)
-	a.Put("/projects/{title}/tasks/{id:[0-9]+}", a.UpdateTask)
-	a.Delete("/projects/{title}/tasks/{id:[0-9]+}", a.DeleteTask)
-	a.Put("/projects/{title}/tasks/{id:[0-9]+}/complete", a.CompleteTask)
-	a.Delete("/projects/{title}/tasks/{id:[0-9]+}/complete", a.UndoTask)
+	a.Get("/projects/{title}/tasks", a.handleRequest(handler.GetAllTasks))
+	a.Post("/projects/{title}/tasks", a.handleRequest(handler.CreateTask))
+	a.Get("/projects/{title}/tasks/{id:[0-9]+}", a.handleRequest(handler.GetTask))
+	a.Put("/projects/{title}/tasks/{id:[0-9]+}", a.handleRequest(handler.UpdateTask))
+	a.Delete("/projects/{title}/tasks/{id:[0-9]+}", a.handleRequest(handler.DeleteTask))
+	a.Put("/projects/{title}/tasks/{id:[0-9]+}/complete", a.handleRequest(handler.CompleteTask))
+	a.Delete("/projects/{title}/tasks/{id:[0-9]+}/complete", a.handleRequest(handler.UndoTask))
 }
 
 // Get wraps the router for GET method
@@ -79,69 +79,15 @@ func (a *App) Delete(path string, f func(w http.ResponseWriter, r *http.Request)
 	a.Router.HandleFunc(path, f).Methods("DELETE")
 }
 
-/*
-** Projects Handlers
- */
-func (a *App) GetAllProjects(w http.ResponseWriter, r *http.Request) {
-	handler.GetAllProjects(a.DB, w, r)
-}
-
-func (a *App) CreateProject(w http.ResponseWriter, r *http.Request) {
-	handler.CreateProject(a.DB, w, r)
-}
-
-func (a *App) GetProject(w http.ResponseWriter, r *http.Request) {
-	handler.GetProject(a.DB, w, r)
-}
-
-func (a *App) UpdateProject(w http.ResponseWriter, r *http.Request) {
-	handler.UpdateProject(a.DB, w, r)
-}
-
-func (a *App) DeleteProject(w http.ResponseWriter, r *http.Request) {
-	handler.DeleteProject(a.DB, w, r)
-}
-
-func (a *App) ArchiveProject(w http.ResponseWriter, r *http.Request) {
-	handler.ArchiveProject(a.DB, w, r)
-}
-
-func (a *App) RestoreProject(w http.ResponseWriter, r *http.Request) {
-	handler.RestoreProject(a.DB, w, r)
-}
-
-/*
-** Tasks Handlers
- */
-func (a *App) GetAllTasks(w http.ResponseWriter, r *http.Request) {
-	handler.GetAllTasks(a.DB, w, r)
-}
-
-func (a *App) CreateTask(w http.ResponseWriter, r *http.Request) {
-	handler.CreateTask(a.DB, w, r)
-}
-
-func (a *App) GetTask(w http.ResponseWriter, r *http.Request) {
-	handler.GetTask(a.DB, w, r)
-}
-
-func (a *App) UpdateTask(w http.ResponseWriter, r *http.Request) {
-	handler.UpdateTask(a.DB, w, r)
-}
-
-func (a *App) DeleteTask(w http.ResponseWriter, r *http.Request) {
-	handler.DeleteTask(a.DB, w, r)
-}
-
-func (a *App) CompleteTask(w http.ResponseWriter, r *http.Request) {
-	handler.CompleteTask(a.DB, w, r)
-}
-
-func (a *App) UndoTask(w http.ResponseWriter, r *http.Request) {
-	handler.UndoTask(a.DB, w, r)
-}
-
 // Run the app on it's router
 func (a *App) Run(host string) {
 	log.Fatal(http.ListenAndServe(host, a.Router))
+}
+
+type RequestHandlerFunction func(db *gorm.DB, w http.ResponseWriter, r *http.Request)
+
+func (a *App) handleRequest(handler RequestHandlerFunction) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		handler(a.DB, w, r)
+	}
 }
